76f3cda20edb7664c1228a02b0bfaf47cbfdca96
Z1at <nikandrov2103@gmail.com>
2022-10-18 16:49:28 +0300

    Eighth commit(Added perror)

--- Solution/src/main.c
+++ Solution/src/main.c
@@ -7,29 +7,36 @@
 #include "openClose/openFile.h"
 #include "rotation/rotate.h"
 
-int main(){
+int main(int cntArg, char* args[]){
+    if(cntArg != 3){
+        perror("incorrectly passed arguments");
+        return -1;
+    }
+
     FILE *file;
-    if (openFile("C:\\Users\\Zlat\\Desktop\\rotationOfPicture\\input\\input.bmp", &file, "rb") != OPEN_OK) {
+    if (openFile(args[1], &file, "rb") != OPEN_OK) {
+        perror("couldn't open the file");
         return -2;
     }
 
     struct image img = {0};
 
     if (fromBmp(file, &img) != READ_OK) {
-        perror("error: bmp reader failed");
+        perror("couldn't convert from bmp");
         return -3;
     }
 
     struct image res = rotate(img);
-    FILE *res_file = NULL;
+    FILE *res_file;
 
-    if (openFile("C:\\Users\\Zlat\\Desktop\\rotationOfPicture\\output\\output.bmp", &res_file, "wb") != OPEN_OK) {
+    if (openFile(args[2], &res_file, "wb") != OPEN_OK) {
+        perror("couldn't open the file");
         imageDestroyer(&res);
         return -2;
     }
 
     if (toBmp(res_file, &res) != WRITE_OK) {
-        perror("error: bmp writer failed");
+        perror("couldn't convert to bmp");
         imageDestroyer(&res);
         closeFile(res_file);
         return -5;
@@ -37,12 +44,12 @@ int main(){
 
 
     if (closeFile(file) != CLOSE_OK) {
-        perror("error: close file failed");
+        perror("couldn't close the file");
         imageDestroyer(&img);
         return -4;
     }
     if (closeFile(res_file) != CLOSE_OK) {
-        perror("error: close file failed");
+        perror("couldn't close the file");
         imageDestroyer(&img);
         return -6;
     }
